f48f786557d039616b887accf5949350f56b1f0e
Henn <27HennG@badinhs.org>
2025-08-26 14:29:20 -0400

    adding more notes and things like that

--- variablesNotes.js
+++ variablesNotes.js
@@ -2,3 +2,8 @@
 // When text says "upstream is gone" the computer believes that there was something there before but doen't know what exactly.
 // "git push origin main" tells GitHub to basically download everything on the computer to save it.
 // the green bar on the left hand side means that the line isn't yet saved but the computer recognizes it
+
+// a variabe is essentialy an invisiable box. You can out whatever you want into the box
+// and then if you "call" the variable, it will tell you what it contains.
+
+// let is a command to create a new variable. It pulls a box out of the aether and gives it a name.
