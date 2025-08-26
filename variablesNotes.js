f5e67d873ed2e1a1a963ab4b187d33445bbf585f
Henn <27HennG@badinhs.org>
2025-08-26 13:56:57 -0400

    just testing everything

--- variablesNotes.js
+++ variablesNotes.js
@@ -1,3 +1,4 @@
 //This is a comment. This is visible to the user, but not to the computer itself. They're very useful.
 // When text says "upstream is gone" the computer believes that there was something there before but doen't know what exactly.
 // "git push origin main" tells GitHub to basically download everything on the computer to save it.
+// hello hello
