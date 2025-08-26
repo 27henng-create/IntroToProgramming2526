53dab1b02393171b9fc1ae7d255a1deb5d45d3c2
Henn <27HennG@badinhs.org>
2025-08-26 14:04:43 -0400

    added a few more notes for something small

--- variablesNotes.js
+++ variablesNotes.js
@@ -1,4 +1,4 @@
 //This is a comment. This is visible to the user, but not to the computer itself. They're very useful.
 // When text says "upstream is gone" the computer believes that there was something there before but doen't know what exactly.
 // "git push origin main" tells GitHub to basically download everything on the computer to save it.
-// hello hello
+// the green bar on the left hand side means that the line isn't yet saved but the computer recognizes it
