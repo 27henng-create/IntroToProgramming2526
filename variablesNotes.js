b9045448c2f9425db7d5f5cab29aa8d9d52cb771
27henng-create <27henng@badinhs.org>
2025-08-28 14:25:27 -0400

    Update variablesNotes.js

--- variablesNotes.js
+++ variablesNotes.js
@@ -7,3 +7,17 @@
 // and then if you "call" the variable, it will tell you what it contains.
 
 // let is a command to create a new variable. It pulls a box out of the aether and gives it a name.
+
+// there are techicaly 3 kinds of variables in javascript
+
+let first = 1;
+const second = 2;
+var third = 3;
+
+// when having two things that are the same like "first = 1" and "first = 1+1" whatever comes second, overrides the first command
+// telling the computer to use the second code over the first code.
+// "const" will not allow you to change that variable since it is constant and it wont allow that varibale to EVER be changed
+// like if you set "const second = 2;" and then try and override that code with "second = 4;" that const will keep the number the same
+
+// OPERATORS: +, -, *, /, ** (exponent), % (modulo/mod)
+// modulo is the remainder of long form of division so 6 % 3 = 0 (3 goes into 6 with a remainder of 0) 
