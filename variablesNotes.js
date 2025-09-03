7124c09ecc06138759dc2c473e376ef31c02bee7
27henng-create <27henng@badinhs.org>
2025-09-03 14:24:16 -0400

    added more notes that I forgot to add

--- variablesNotes.js
+++ variablesNotes.js
@@ -1,5 +1,6 @@
 //This is a comment. This is visible to the user, but not to the computer itself. They're very useful.
-// When text says "upstream is gone" the computer believes that there was something there before but doen't know what exactly.
+// When text says "upstream is gone" the computer believes that there was something there before
+// but doen't know what exactly.
 // "git push origin main" tells GitHub to basically download everything on the computer to save it.
 // the green bar on the left hand side means that the line isn't yet saved but the computer recognizes it
 
@@ -29,10 +30,16 @@
 // console.log(second);
 // console.log(third);
 
-// when having two things that are the same like "first = 1" and "first = 1+1" whatever comes second, overrides the first command
-// telling the computer to use the second code over the first code.
-// "const" will not allow you to change that variable since it is constant and it wont allow that varibale to EVER be changed
-// like if you set "const second = 2;" and then try and override that code with "second = 4;" that const will keep the number the same
+// when having two things that are the same like "first = 1" and "first = 1+1" whatever comes second
+// overrides the first command telling the computer to use the second code over the first code.
+
+// "const" will not allow you to change that variable since it is 
+// constant and it wont allow that varibale to EVER be changed
+// like if you set "const second = 2;" and then try and override that code with 
+// "second = 4;" that const will keep the number the same
+
+// having "first = 1" and "let first = 10" on the html (remember to press F12) it will show both numbers
+
 
 // OPERATORS: +, -, *, /, ** (exponent), % (modulo/mod)
 // modulo is the remainder of long form of division so 6 % 3 = 0 (3 goes into 6 with a remainder of 0) 
