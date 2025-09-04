91cbab2ba2b87497215e0328db540044fcde68e7
27henng-create <27henng@badinhs.org>
2025-09-04 14:30:27 -0400

    added notes to the file

--- dataTypeNotes.js
+++ dataTypeNotes.js
@@ -1,2 +1,10 @@
 // these are notes on data types and conditionals in javascript
 
+//javascript is a dynamically typed language. C/java/C++ are statically typed.
+// in a dynamically type language, you can change what "type" of value is stored
+// in a given value.
+
+//example:
+let message = "hello"
+message = 123;
+
