f0f90e11360376141def9d9d9478285960368be2
27henng-create <27henng@badinhs.org>
2025-09-10 14:28:07 -0400

    notes on string methods, day 1

--- dataTypeNotes.js
+++ dataTypeNotes.js
@@ -74,3 +74,51 @@ console.log(fish)
 
 
 // Practice with strings:
+// Quick vocabulary distiction to make: functions vs. methods.
+// functions are "generic".
+// a method is a funciton that is bound to a specific data type.
+// functions are usually called by stating the name of the function, and
+// what you want it applied to.     e.g. average(dataSet)
+// whereas a method usually follows whit it is being applied to
+// e.g. text.at(2)
+
+// here are several string methods:
+
+// length (technically a property, not strictly a method)
+
+let text = "IHUGFYTDRXCFGHJVBUGFDRXFCHGVJBUG"
+console.log(text.length);
+
+// ways to extract characters from a string:
+// charAt(): returns the character at the indicated "slot" in the string
+
+let text = "Stevie T. Badin";
+console.log(text.charAt(3));    // this means it would be the "v" because it starts from 0 and
+// keeps going up like 0, 1, 2, 3, and so on and so on
+
+// charCodeAt(): Returns the UTF-16's code of the character at the specificed index (in console)
+console.log(text.charCodeAt(3));
+
+// you can't use many charCodeAt() for many symbols for e.g. "Badin" cosonle.log(charCodeAt(3,4))
+// THIS IS NOT POSSIBLE
+
+// .at() is a more usefu and preferable option to charAt();
+// console.log(text.at(-3)) = the 4th to last letter in the whole "phrase" so it goes backwards
+
+// .concat()    stands for concatenation, allows you to concatenate 2 or more strings
+let text1 = "Hello";
+let text2 = "Grant";
+
+text3 = text1.concat(" ", text2);   // you can put whatever you want inbetween the apostrophes. a.k.a string
+console.log(text3);
+
+text4 = text1 + text2;
+console.log(text4) //same thing as before, you can add a string inbetween any of the texts
+// to then add anything you want inbetween the strings.
+
+// if you want to extract a "substring" (piece of a larger string) you can use three methods:
+// .slice()     e.g. "text.slice(3,8)" returns a string that starts from the given 
+// index and ends befeore the secon index
+console.log(text.slice(3,8));
+
+// if you
