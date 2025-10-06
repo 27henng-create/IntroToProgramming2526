5ee6f1b886a69831a91b4bee2b4b68b634d23b07
27henng-create <27henng@badinhs.org>
2025-10-06 14:28:50 -0400

    did some problems for classroom

---
+++ functionsPractice.js
@@ -0,0 +1,26 @@
+// function multiply( a, b){
+//     let m = (a * b)
+//     return(m)
+// }
+// console.log(multiply(2,5))
+
+// function checkAge(age) { 
+//       if (age > 18) { 
+//            return true; 
+//        } else { 
+//            return confirm('Did parents allow you?');
+//     }
+// }
+
+// console.log(checkAge(18))
+
+// function checkAge(age) { 
+//     if (age > 18) { 
+//        return true;
+// } 
+// return confirm('Did parents allow you?');
+// }
+
+// console.log(checkAge(18))
+
+// the function will not run differently if you remove the "else"
