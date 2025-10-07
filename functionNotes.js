4e3506a7596fd9e37528558795b0b89312f10807
27henng-create <27henng@badinhs.org>
2025-10-07 13:19:01 -0400

    added notes about loops

--- functionNotes.js
+++ functionNotes.js
@@ -96,3 +96,17 @@ let welcome = (age < 18) ?
 
 welcome();
 
+// Loop : repeats a chunk of code while some conditions are true
+
+// whie loop: make a fibonacci generator
+let i = 1; // steps to do
+let a = 0;
+let b = 1;
+
+// 10th step of the fibonacci seq
+while(i < 10){
+    let c = a + b;
+    a = b;
+    b = c;
+    i = i + 1;      // i +=1 is equal to i = i + 1 (i++ is also vaible)
+}
--- functionsPractice.js
+++ functionsPractice.js
@@ -1,9 +1,11 @@
-// function multiply( a, b){
+// 1)
+//function multiply( a, b){
 //     let m = (a * b)
 //     return(m)
 // }
 // console.log(multiply(2,5))
 
+// 4)
 // function checkAge(age) { 
 //       if (age > 18) { 
 //            return true; 
@@ -24,3 +26,24 @@
 // console.log(checkAge(18))
 
 // the function will not run differently if you remove the "else"
+
+// 5)
+// let age = 19
+// let chosenAge = (age > 18) ? " true " : " false" 
+// console.log(chosenAge)
+
+// 6)
+// function min(a,b){
+//     if( a < b){
+//         return a
+//     } else {
+//         return b;
+//     }
+// }
+// console.log(3,5)
+
+// 7)
+function pow(x,n){
+    return x**n;
+}
+console.log(pow(3,2))
